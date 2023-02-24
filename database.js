14e5a75a1438d3d448ff55883324195f926fd9b4
Nohemi Morales <nohemi@umail.ucsb.edu>
2023-02-24 11:28:21 -0800

    implemented sqlite database setup

--- database.js
+++ database.js
@@ -6,15 +6,36 @@ let db = new sqlite3.Database('book-log.db', (err) => {
         throw err;
     }
     else {
+        // Build both tables
         console.log('CONNECTING TO book-log DATABASE ...');
-        // use db.run to make schema for our database (still working on design)
+        db.run(`CREATE TABLE authors (
+            id INT PRIMARY KEY AUTOINCREMENT,
+            name TEXT)`, (err) => {
+                if(err) {
+                    console.log('authors TABLE ALREADY CREATED');
+                }
+            });
+        db.run(`CREATE TABLE books (
+            ISBN INT PRIMARY KEY,
+            title TEXT,
+            author_id INT FOREIGN KEY REFERENCES authors(id),
+            qty INT)`, (err) => {
+                if(err) {
+                    console.log('books TABLE ALREADY CREATED')
+                }
+            });
         /*
+        CREATE TABLE authors (
+            id INT PRIMARY KEY,
+            name TEXT);
         CREATE TABLE books (
             id INT PRIMARY KEY,
             ISBN INT,
             title TEXT,
             author_id INT FOREIGN KEY REFERENCES authors(id),
-            qty INT)
+            qty INT);
         */
     }
 });
+
+module.exports = db;
